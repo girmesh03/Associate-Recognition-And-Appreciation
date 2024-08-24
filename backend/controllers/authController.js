11cf68ffc6988b4c3222783aee4e29e0b174374c
girmesh03 <girmachewzewdie03@gmail.com>
2024-08-24 23:45:21 +0300

    feat(auth): finalize login and signup routes with token handling, cookies, and integrated custom error handling

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,11 +1,90 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+import {
+  generateAccessToken,
+  generateRefreshToken,
+} from "../utils/generateToken.js";
+import CustomError from "../utils/CustomError.js";
 
+//@desc     Register new user
+//@route    POST /api/auth/signup
+//@access   Public
 const signup = asyncHandler(async (req, res, next) => {
-  res.status(201).json({ message: "signup" });
+  const { firstName, lastName, department, position, email, password } =
+    req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    const error = new CustomError("User already exists", 400);
+    return next(error);
+  }
+
+  const user = await User.create({
+    firstName,
+    lastName,
+    department,
+    position,
+    email,
+    password,
+  });
+
+  const accessToken = generateAccessToken(user._id, user.role);
+  const refreshToken = generateRefreshToken(user._id, user.role);
+
+  res.cookie("access_token", accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
+  });
+
+  res.cookie("refresh_token", refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
+
+  const { password: pwd, __v, ...others } = user._doc;
+
+  res.status(201).json(others);
 });
 
+//@desc     Login user
+//@route    POST /api/auth/login
+//@access   Public
 const login = asyncHandler(async (req, res, next) => {
-  res.status(200).json({ message: "login" });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    const error = new CustomError("You don't have an account, sign up", 401);
+    return next(error);
+  } else if (user && !(await user.matchPassword(password))) {
+    const error = new CustomError("Invalid email or password", 401);
+    return next(error);
+  }
+
+  const accessToken = generateAccessToken(user._id, user.role);
+  const refreshToken = generateRefreshToken(user._id, user.role);
+
+  res.cookie("access_token", accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
+  });
+
+  res.cookie("refresh_token", refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
+
+  const { password: pwd, __v, ...others } = user._doc;
+
+  res.status(200).json(others);
 });
 
 export { signup, login };
