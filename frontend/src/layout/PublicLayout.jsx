e39d59735e453d62a710aa11f999cf6b0f43c97d
girmesh03 <girmachewzewdie03@gmail.com>
2024-08-24 03:02:17 +0300

    refactor(PublicLayout): add dynamic height adjustment and overflow handling

--- frontend/src/layout/PublicLayout.jsx
+++ frontend/src/layout/PublicLayout.jsx
@@ -1,12 +1,23 @@
 import { Outlet } from "react-router-dom";
 import { Box } from "@mui/material";
-import PublicNavigation from "../components/PublicComponents/PublicNavigation";
+import PublicNavigation from "../components/publicComponents/PublicNavigation";
 
 const PublicLayout = () => {
   return (
-    <Box width="100%" height="100%">
+    <Box width="100%" height="100%" sx={{ overflowY: "auto" }}>
       <PublicNavigation />
-      <Outlet />
+      <Box
+        sx={{
+          width: "100%",
+          height: {
+            xs: "calc(100% - 48px)",
+            sm: "calc(100% - 64px)",
+            md: "calc(100% - 80px)",
+          },
+        }}
+      >
+        <Outlet />
+      </Box>
     </Box>
   );
 };
