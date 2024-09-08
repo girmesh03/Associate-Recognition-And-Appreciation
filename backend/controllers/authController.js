fa57db74cef4b83d317beec637d8d116993e5d84
girmesh03 <girmachewzewdie03@gmail.com>
2024-09-09 01:00:24 +0300

    feat(auth): update signup, login, logout, and refresh token flow
    - Added role to signup.
    - Improved token expiration handling for refresh tokens.
    - Updated login to handle missing email or password.
    - Changed logout to DELETE method.
    - Enhanced refresh token logic with better error handling.

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,25 +1,27 @@
-import asyncHandler from "express-async-handler";
 import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 import RefreshToken from "../models/refreshTokenModel.js";
+import CustomError from "../utils/CustomError.js";
 import {
   generateAccessToken,
   generateRefreshToken,
 } from "../utils/generateToken.js";
-import CustomError from "../utils/CustomError.js";
 
 //@desc     Register new user
 //@route    POST /api/auth/signup
 //@access   Public
 const signup = asyncHandler(async (req, res, next) => {
-  const { firstName, lastName, department, position, email, password } =
+  const { firstName, lastName, department, position, email, password, role } =
     req.body;
 
+  // Check if user already exists, if yes, throw an error
   const userExists = await User.findOne({ email });
   if (userExists) {
     return next(new CustomError("User already exists", 409));
   }
 
+  // Create new user
   const user = await User.create({
     firstName,
     lastName,
@@ -27,13 +29,25 @@ const signup = asyncHandler(async (req, res, next) => {
     position,
     email,
     password,
+    role,
   });
 
+  // Generate JWT
   const accessToken = generateAccessToken(user._id, user.role);
   const refreshToken = generateRefreshToken(user._id, user.role);
 
-  await RefreshToken.create({ token: refreshToken, userId: user._id });
+  // Save refresh token
+  const savedRefreshToken = await RefreshToken.create({
+    token: refreshToken,
+    userId: user._id,
+    expireAt: new Date(Date.now() + 30000),
+  });
 
+  if (!savedRefreshToken) {
+    return next(new CustomError("Something went wrong", 500));
+  }
+
+  // Send refresh token to client, via a cookie
   res.cookie("refresh_token", refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
@@ -41,8 +55,10 @@ const signup = asyncHandler(async (req, res, next) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
+  // Prepare response
   const { password: pwd, __v, ...others } = user._doc;
 
+  // Send response
   res.status(201).json({ accessToken, user: others });
 });
 
@@ -52,18 +68,39 @@ const signup = asyncHandler(async (req, res, next) => {
 const login = asyncHandler(async (req, res, next) => {
   const { email, password } = req.body;
 
+  // Check if email and password are provided
+  if (!email || !password) {
+    return next(new CustomError("Please provide an email and password", 400));
+  }
+
+  // Check if user exists, if not, throw an error
   const user = await User.findOne({ email }).select("+password");
   if (!user) {
     return next(new CustomError("No account found. Please sign up.", 404));
   } else if (!(await user.matchPassword(password))) {
-    return next(new CustomError("Invalid email or password", 401));
+    return next(new CustomError("Invalid email or password", 403));
   }
 
+  // Generate JWT
   const accessToken = generateAccessToken(user._id, user.role);
   const refreshToken = generateRefreshToken(user._id, user.role);
 
-  await RefreshToken.create({ token: refreshToken, userId: user._id });
+  // Save refresh token
+  const updatedToken = await RefreshToken.findOneAndUpdate(
+    { userId: user._id },
+    {
+      token: refreshToken,
+      expireAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    },
+    { new: true, upsert: true }
+  );
 
+  // If there is an error updating the refresh token, return an error
+  if (!updatedToken) {
+    return next(new CustomError("Error updating refresh token.", 500));
+  }
+
+  // Send refresh token to client, via a cookie
   res.cookie("refresh_token", refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
@@ -71,54 +108,62 @@ const login = asyncHandler(async (req, res, next) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
+  // Prepare response
   const { password: pwd, __v, ...others } = user._doc;
 
+  // Send response
   res.status(200).json({ accessToken, user: others });
 });
 
 //@desc     Logout user
-//@route    POST /api/auth/logout
-//@access   Public
+//@route    DELETE /api/auth/logout
+//@access   public
 const logout = asyncHandler(async (req, res, next) => {
   const refreshToken = req.cookies?.refresh_token;
 
+  // If there is refresh token update it
   if (refreshToken) {
-    // Remove refresh token from the database
-    await RefreshToken.deleteOne({ token: refreshToken });
-    res.clearCookie("refresh_token");
+    await RefreshToken.findOneAndDelete({ token: refreshToken });
   }
 
+  // Clear the refresh token cookie, return an empty response
+  res.clearCookie("refresh_token");
   res.sendStatus(204);
 });
 
 //@desc     Refresh access token
-//@route    POST /api/auth/refresh
-//@access   Public
+//@route    GET /api/auth/refresh
+//@access   public
 const refreshAccessToken = asyncHandler(async (req, res, next) => {
   const refreshToken = req.cookies?.refresh_token;
 
   if (!refreshToken) {
-    return next(new CustomError("Authentication required", 401));
+    // console.log("No refresh token provided");
+    return next(new CustomError("Unauthorized, Please login", 403));
   }
 
   const validToken = await RefreshToken.findOne({ token: refreshToken });
 
   if (!validToken) {
-    return next(new CustomError("Authentication required", 401));
+    // console.log("No refresh token found");
+    return next(new CustomError("Unauthorized, Please login", 403));
   }
 
-  jwt.verify(
-    refreshToken,
-    process.env.JWT_REFRESH_SECRET,
-    async (err, decoded) => {
-      if (err) {
-        return next(new CustomError("Authentication required", 401));
-      }
-      const accessToken = generateAccessToken(decoded.id, decoded.role);
+  const user = await User.findById(validToken.userId);
+  if (!user) {
+    // console.log("User not found for refresh token");
+    return next(new CustomError("Unauthorized, Please login", 403));
+  }
 
-      res.status(200).json({ accessToken });
+  jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET, (err, decoded) => {
+    if (err) {
+      // console.error("Refresh token verification error:", err);
+      return next(new CustomError("Unauthorized, Please login", 403));
     }
-  );
+
+    const accessToken = generateAccessToken(decoded.id, decoded.role);
+    res.status(200).json({ accessToken });
+  });
 });
 
 export { signup, login, logout, refreshAccessToken };
