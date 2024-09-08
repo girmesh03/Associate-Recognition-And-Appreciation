00c7e68c94c0a3a9747ec818fd8c4fce3fa206a4
girmesh03 <girmachewzewdie03@gmail.com>
2024-09-09 01:27:47 +0300

    feat(comment): refactor comment schema and add postType validation
    - Refactored comment schema to include validation and trim options.
    - Added  field with enum validation for recognition, nomination, and appreciation post types.
    - Improved error messages for required fields, including user, postId, and comment.

---
+++ backend/models/commentModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: [true, "User id is required."],
+  },
+  comment: {
+    type: String,
+    required: [true, "Comment is required"],
+    maxlength: [200, "Maximum 200 characters allowed"],
+    trim: true,
+  },
+  postType: {
+    type: String,
+    enum: {
+      values: ["recognition", "nomination", "appreciation"],
+      message: `{VALUE} is not a valid post type!`,
+    },
+    required: [true, "Post type is required"],
+  },
+  postId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: [true, "Post id is required."],
+  },
+});
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+export default Comment;
