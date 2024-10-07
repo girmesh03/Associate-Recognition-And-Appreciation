0824a912c7e9cfb9812f13ff6d0adac17db569b7
girmesh03 <girmachewzewdie03@gmail.com>
2024-10-07 20:05:12 +0300

    feat(Navbar): refactor component structure and enhance navigation
    - Replaced AppBar and Toolbar with styled components for better styling control.
    - Implemented a responsive Drawer for mobile navigation.
    - Added public navigation items with smooth scrolling functionality.
    - Included logout functionality for authenticated users and conditional rendering for navigation options.
    - Improved accessibility with keyboard event handling for the Drawer.
    - Adjusted styles for better visual consistency and user experience.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,110 +1,237 @@
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+// react import
+import { useState } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+
+// mui import
+import { styled, alpha } from "@mui/material/styles";
 import {
   AppBar,
-  Badge,
-  Box,
-  IconButton,
-  Stack,
   Toolbar,
+  IconButton,
+  Drawer,
+  Box,
   Typography,
+  Stack,
+  Badge,
+  Button,
+  MenuItem,
+  useMediaQuery,
 } from "@mui/material";
-
-import { useSelector, useDispatch } from "react-redux";
-import { toggleMode } from "../redux/features/auth/authSlice";
-
+import MenuIcon from "@mui/icons-material/Menu";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import NotificationsIcon from "@mui/icons-material/NotificationsOutlined";
 import SearchIcon from "@mui/icons-material/SearchOutlined";
-import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import FeaturesIcon from "@mui/icons-material/AutoAwesome";
+import TestimonialsIcon from "@mui/icons-material/ThumbUp";
+import HighlightsIcon from "@mui/icons-material/Star";
+import PricingIcon from "@mui/icons-material/AttachMoney";
+import FAQIcon from "@mui/icons-material/HelpOutline";
 
+// redux import
+import { useSelector, useDispatch } from "react-redux";
+import { toggleMode } from "../redux/features/auth/authSlice";
+import { logout } from "../redux/features/auth/authActions";
+
+// components
+import Sidebar from "./Sidebar";
+
+// utils
 import { drawerWidth } from "../utils/constants";
 
-const Navbar = ({ isNoneMobile, handleDrawerToggle }) => {
-  const mode = useSelector((state) => state.auth.mode);
+// Styled components
+const StyledAppBar = styled(AppBar)({
+  boxShadow: "none",
+  backgroundColor: "transparent",
+  backgroundImage: "none",
+});
+
+const StyledToolbar = styled(Toolbar)(({ theme }) => ({
+  justifyContent: "space-between",
+  "& .MuiSvgIcon-root": {
+    color: theme.palette.text.primary,
+    fontSize: "1.25rem",
+  },
+}));
+
+const StyledLogoText = styled(Typography)(({ theme }) => ({
+  fontWeight: 700,
+  letterSpacing: theme.spacing(3 / 8),
+  fontSize: "clamp(1.5rem, 1vw, 2rem)",
+  color:
+    theme.palette.mode === "light" ? theme.palette.primary.main : "inherit",
+  cursor: "pointer",
+  display: "block",
+  [theme.breakpoints.down("sm")]: {
+    display: "none",
+    "@media (orientation: landscape)": {
+      display: "block",
+    },
+  },
+  [theme.breakpoints.up("md")]: {
+    display: "block",
+  },
+}));
+
+const StyledDrawer = styled(Drawer)(({ theme }) => ({
+  height: "100vh",
+  width: drawerWidth,
+  "& .MuiDrawer-paper": {
+    width: drawerWidth,
+    boxSizing: "border-box",
+    backgroundColor: "background.paper",
+    backgroundImage:
+      theme.palette.mode === "light"
+        ? "linear-gradient(180deg, #CEE5FD, #FFF)"
+        : `linear-gradient(#02294F, ${alpha("#090E10", 0.0)})`,
+    backgroundRepeat: "no-repeat",
+  },
+}));
+
+// public nav items
+const publicNavListItems = [
+  { title: "Features", icon: <FeaturesIcon />, name: "features" },
+  { title: "Testimonials", icon: <TestimonialsIcon />, name: "testimonials" },
+  { title: "Highlights", icon: <HighlightsIcon />, name: "highlights" },
+  { title: "Pricing", icon: <PricingIcon />, name: "pricing" },
+  { title: "FAQ", icon: <FAQIcon />, name: "faq" },
+];
+
+const Navbar = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const { isAuthenticated, mode } = useSelector((state) => state.auth);
+  const isNoneMobile = useMediaQuery((theme) => theme.breakpoints.up("md"));
+  const route = useLocation().pathname;
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setIsDrawerOpen(open);
+  };
+
+  const scrollToSection = (sectionId) => {
+    const sectionElement = document.getElementById(sectionId);
+    if (sectionElement) {
+      sectionElement.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/", { replace: true });
+  };
 
   return (
-    <AppBar
-      position="fixed"
-      sx={{
-        boxShadow: 0,
-        bgcolor: "transparent",
-        backgroundImage: "none",
-      }}
-    >
-      <Box
-        sx={{
-          maxWidth: "xl",
-          width: "100%",
-          margin: "0 auto",
-          pl: { md: `${drawerWidth}px` },
-        }}
-      >
-        <Toolbar
-          variant="regular"
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            "& .MuiSvgIcon-root": {
-              color: "text.primary",
-              fontSize: "20px",
-            },
-          }}
-        >
-          <Stack direction="row" alignItems="center" gap={1}>
-            <IconButton
-              onClick={handleDrawerToggle(true)}
-              variant="outlined"
-              size="small"
-              sx={{ display: isNoneMobile ? "none" : "" }}
-            >
-              <MenuRoundedIcon />
-            </IconButton>
-            <Typography
-              variant="h4"
-              component={Link}
-              to="/"
-              sx={{
-                fontWeight: 700,
-                letterSpacing: 3,
-                fontSize: "clamp(1.5rem, 1vw, 2rem)",
-                color: mode === "light" ? "primary.main" : "text.primary",
-                textDecoration: "none",
-                display: { xs: "none", sm: "block" },
-              }}
-            >
-              Monthmaster
-            </Typography>
-          </Stack>
-          <Stack alignItems="center" direction="row">
-            <IconButton onClick={() => {}}>
-              <SearchIcon
-                sx={{
-                  transform: "translateX(2px) translateY(1.5px)",
-                }}
-              />
-            </IconButton>
-            <IconButton onClick={() => {}}>
-              <Badge>
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-            <IconButton onClick={() => dispatch(toggleMode())}>
-              {mode === "light" ? <DarkModeIcon /> : <LightModeIcon />}
-            </IconButton>
-          </Stack>
-        </Toolbar>
-      </Box>
-    </AppBar>
-  );
-};
+    <StyledAppBar position="sticky">
+      <StyledToolbar>
+        <Stack direction="row" alignItems="center" gap={1}>
+          <IconButton
+            onClick={toggleDrawer(true)}
+            sx={{ display: { md: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <StyledLogoText
+            variant="h4"
+            onClick={() => navigate(isAuthenticated ? "/recognitions" : "/")}
+          >
+            Monthmaster
+          </StyledLogoText>
+        </Stack>
 
-Navbar.propTypes = {
-  isNoneMobile: PropTypes.bool.isRequired,
-  handleDrawerToggle: PropTypes.func.isRequired,
+        {isNoneMobile && route === "/" && (
+          <Stack
+            direction="row"
+            alignItems="center"
+            flexGrow={1}
+            sx={{ mx: 1, display: { xs: "none", md: "flex" } }}
+          >
+            {publicNavListItems.map((item) => (
+              <MenuItem
+                key={item.title}
+                onClick={() => scrollToSection(item.name)}
+              >
+                <Typography variant="body2" color="text.primary">
+                  {item.title}
+                </Typography>
+              </MenuItem>
+            ))}
+          </Stack>
+        )}
+
+        <Stack direction="row" alignItems="center" gap={1}>
+          <IconButton onClick={() => dispatch(toggleMode())}>
+            {mode === "light" ? <DarkModeIcon /> : <LightModeIcon />}
+          </IconButton>
+          {isAuthenticated ? (
+            <>
+              <IconButton onClick={() => {}}>
+                <SearchIcon
+                  sx={{ transform: "translateX(2px) translateY(1.5px)" }}
+                />
+              </IconButton>
+              <IconButton onClick={() => {}}>
+                <Badge>
+                  <NotificationsIcon />
+                </Badge>
+              </IconButton>
+              <Button
+                color="primary"
+                variant="outlined"
+                size="small"
+                onClick={handleLogout}
+              >
+                Logout
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button
+                color="primary"
+                variant="text"
+                size="small"
+                component={Link}
+                to="/login"
+              >
+                Sign in
+              </Button>
+              <Button
+                color="primary"
+                variant="contained"
+                size="small"
+                component={Link}
+                to="/signup"
+              >
+                Sign up
+              </Button>
+            </>
+          )}
+        </Stack>
+      </StyledToolbar>
+
+      <StyledDrawer
+        anchor="left"
+        open={isDrawerOpen}
+        onClose={toggleDrawer(false)}
+        ModalProps={{ keepMounted: true }}
+      >
+        <Box
+          role="presentation"
+          onClick={toggleDrawer(false)}
+          onKeyDown={toggleDrawer(false)}
+          sx={{ height: "100%", display: "flex", flexDirection: "column" }}
+        >
+          <Sidebar />
+        </Box>
+      </StyledDrawer>
+    </StyledAppBar>
+  );
 };
 
 export default Navbar;
