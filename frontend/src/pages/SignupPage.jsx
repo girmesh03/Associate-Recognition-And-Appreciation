f5257bca835ca83f6ea3cbd196634b4b06e67d66
girmesh03 <girmachewzewdie03@gmail.com>
2024-10-07 19:45:32 +0300

    refactor: enhance SignupPage component with styled components
    - Replaced inline styles with styled components for improved readability and maintainability.
    - Updated layout to include a welcoming message emphasizing community values.
    - Improved responsiveness and padding adjustments for smaller screens.
    - Refactored link typography for consistency and improved styling.

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,69 +1,104 @@
+// react import
 import { Link } from "react-router-dom";
-import { Box, Paper, Typography } from "@mui/material";
+
+// mui import
+import { Box, Paper, Stack, Typography, styled } from "@mui/material";
+
+// components
 import SignupForm from "../components/formComponents/SignupForm";
+
+// Styled components
+const SignupContainer = styled(Box)(({ theme }) => ({
+  height: "100%",
+  overflowY: "auto",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: theme.spacing(3, 0),
+}));
+
+// Styled components
+const ContentBox = styled(Box)(({ theme }) => ({
+  flexGrow: 1,
+  width: "100%",
+  maxWidth: 800,
+  alignSelf: "center",
+  justifyContent: "center",
+  display: "flex",
+  flexDirection: "column",
+  padding: theme.spacing(2),
+  [theme.breakpoints.down("sm")]: {
+    padding: theme.spacing(0.5),
+    maxWidth: 500,
+  },
+}));
+
+const StyledPaper = styled(Paper)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  gap: theme.spacing(2),
+  boxShadow: "none",
+  backgroundColor: "transparent",
+  border: `1px solid ${theme.palette.divider}`,
+  borderRadius: theme.shape.borderRadius * 4,
+  padding: theme.spacing(6, 4),
+  [theme.breakpoints.down("sm")]: {
+    padding: theme.spacing(4, 1),
+    "@media screen and (min-width: 568px)": {
+      padding: theme.spacing(6, 2),
+      border: `1px solid ${theme.palette.divider}`,
+    },
+  },
+}));
+
+const StyledTypographyLink = styled(Typography)(({ theme }) => ({
+  color: "inherit",
+  textDecoration: "none",
+  cursor: "pointer",
+  "& span": {
+    color: theme.palette.primary.main,
+    marginLeft: theme.spacing(0.75),
+    cursor: "pointer",
+  },
+}));
 
 const SignupPage = () => {
   return (
-    <Box
-      sx={{
-        minHeight: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Paper
-        sx={{
-          borderRadius: 3,
-          boxShadow: 0,
-          backgroundColor: "transparent",
-          border: "1px solid",
-          borderColor: "divider",
-          width: { xs: "100%", sm: "auto" },
-          m: 1,
-        }}
-      >
-        <Box
-          sx={{
-            maxWidth: 700,
-            margin: "0 auto",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            gap: 4,
-            py: 5,
-          }}
-        >
-          <Typography
-            variant="h5"
-            textAlign="center"
-            sx={{ fontWeight: "bold", letterSpacing: 3 }}
-          >
-            Signup
-          </Typography>
+    <SignupContainer>
+      <ContentBox>
+        <StyledPaper>
+          <Stack direction="column" gap={2} sx={{ mb: 2 }}>
+            <Typography
+              variant="h5"
+              textAlign="center"
+              sx={{ fontWeight: "bold", letterSpacing: 3 }}
+            >
+              SIGN UP
+            </Typography>
+            <Typography
+              variant="body1"
+              textAlign="center"
+              color="text.secondary"
+            >
+              Be Part of a Community That Values Recognition and Appreciation.
+              Sign Up Today!
+            </Typography>
+          </Stack>
 
           <SignupForm />
 
-          <Typography
-            textAlign="center"
-            sx={{ color: "inherit", textDecoration: "none" }}
+          <StyledTypographyLink
+            className="login-link"
             component={Link}
             to="/login"
           >
             Already have an account?
-            <span
-              style={{
-                color: "#006fff",
-                marginLeft: "0.5rem",
-                cursor: "pointer",
-              }}
-            >
-              Login
-            </span>
-          </Typography>
-        </Box>
-      </Paper>
-    </Box>
+            <span>Login</span>
+          </StyledTypographyLink>
+        </StyledPaper>
+      </ContentBox>
+    </SignupContainer>
   );
 };
 
